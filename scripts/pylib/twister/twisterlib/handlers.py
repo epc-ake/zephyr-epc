52e6558eda04b5abfd417ec4ff369c7d5f2f9fb7
Armin <ake@espros.com>
2025-02-05 17:16:11 +0100

    scripts: twister: fix flash to esp32 using esp32 runner
    Twister and pytest now flash to the device specified with board_id.
    
    Signed-off-by: Armin Kessler <ake@espros.com>

--- scripts/pylib/twister/twisterlib/handlers.py
+++ scripts/pylib/twister/twisterlib/handlers.py
@@ -530,14 +530,13 @@ class DeviceHandler(Handler):
 
                 board_id = hardware.probe_id or hardware.id
                 product = hardware.product
-                serial_port = hardware.serial
                 if board_id is not None:
                     if runner in ("pyocd", "nrfjprog", "nrfutil"):
                         command_extra_args.append("--dev-id")
                         command_extra_args.append(board_id)
                     elif runner == "esp32":
                         command_extra_args.append("--esp-device")
-                        command_extra_args.append(serial_port)
+                        command_extra_args.append(board_id)
                     elif runner == "openocd" and product == "STM32 STLink":
                         command_extra_args.append("--cmd-pre-init")
                         command_extra_args.append("hla_serial %s" % board_id)
